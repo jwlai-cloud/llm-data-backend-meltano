a584be8cf590cdc68e7164f65f0dd97aa11ba31d
Sven Balnojan <43072233+sbalnojan@users.noreply.github.com>
2023-08-28 12:04:46 +0000

    clean up processing

--- mappers/clean_text.py
+++ mappers/clean_text.py
@@ -8,7 +8,9 @@ class Mapper():
 
     def map_record_message(self, message_dict: dict) -> t.Iterable[Message]:
         page_content = message_dict["record"]["page_content"]
-        text_nl = " ".join(page_content.split("\n"))
+        page_content = page_content.replace("Three Data Point Thursday", "").replace("Finish Slime","")
+        no_unicode = page_content.encode("ascii", "ignore").decode()
+        text_nl = " ".join(no_unicode.split("\n"))
         text_spaces = " ".join(text_nl.split())
         message_dict["record"]["page_content"] = text_spaces
         return message_dict
